1d22a16ede66188f7c6681d63d06a6f255f6a0af
Leonardo Arroyo <contato@leonardoarroyo.com>
2015-11-05 03:28:43 -0200

    Final touches

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,64 +10,22 @@ $(document).ready(function() {
       }, 200);
   })
 
-  /** Lines **/
-  var v1_1 = new Vivus('v1-1', {type: 'scenario-sync', duration: 60, start: 'autostart', animTimingFunction: Vivus.EASE});
-  var v1_2 = new Vivus('v1-2', {type: 'scenario-sync', duration: 60, start: 'autostart', animTimingFunction: Vivus.EASE});
-  var v2 = new Vivus('v2', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v3_1 = new Vivus('v3-1', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v3_2 = new Vivus('v3-2', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v4_1 = new Vivus('v4-1', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v4_2 = new Vivus('v4-2', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v5 = new Vivus('v5', {type: 'delayed', duration: 100, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v6_1_1 = new Vivus('v6-1-1', {type: 'delayed', duration: 30, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v6_1_2 = new Vivus('v6-1-2', {type: 'delayed', duration: 30, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v6_1_3 = new Vivus('v6-1-3', {type: 'delayed', duration: 30, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v6_2_1 = new Vivus('v6-2-1', {type: 'delayed', duration: 30, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v6_2_2 = new Vivus('v6-2-2', {type: 'delayed', duration: 30, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v7_1 = new Vivus('v7-1', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  var v7_2 = new Vivus('v7-2', {type: 'delayed', duration: 60, start: 'manual', animTimingFunction: Vivus.EASE});
-  
   /** Video Player **/
   setupVideoPlayer();
 
   /** Menu **/
   $('.menu').autoMenu({
     1: function() {
-      v2.play();
     },
     2: function() {
-      v3_1.play();
-      v3_2.play();
     },
     3: function() {
-      console.log('oi');
-      v4_1.play();
-      v4_2.play();
-      setTimeout(function() {
-        v5.play();
-
-        setTimeout(function() {
-          v6_1_1.play();
-        }, 550);
-        setTimeout(function() {
-          v6_2_1.play();
-        }, 700);
-        setTimeout(function() {
-          v6_1_2.play();
-        }, 850);
-        setTimeout(function() {
-          v6_2_2.play();
-        }, 1000);
-        setTimeout(function() {
-          v6_1_3.play();
-        }, 1150);
-      }, 600);
     },
     4: function() {
-      v7_1.play();
-      v7_2.play();
+      $('.help').fadeOut();
     }
   });
+
   $('.menu a').click(function(e) {
     e.preventDefault();
     var target = $(e.target).attr('href');
